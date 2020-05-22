0e560e0f532601ccab0447db950e60862ea065ed
Pachec00 <65049200+Pachec00@users.noreply.github.com>
2020-05-22 20:17:42 +0200

    Add files via upload
    Bot de andaluh para discord

---
+++ andaluh-discord/bot.py
@@ -0,0 +1,24 @@
+import requests
+import json
+import discord
+from discord.ext import commands
+
+
+bot = commands.Bot(command_prefix='/')
+
+
+
+
+@bot.command()
+async def an(ctx,p):
+    str = requests.get('https://api.andaluh.es/epa', params=dict(spanish= p , escapeLinks=True, vaf=u'z')).json()
+    await ctx.send(str['andaluh'])
+
+#prueba de commando
+@bot.command()
+async def foo(ctx, arg):
+    await ctx.send(arg)
+    print(arg)
+    print(type(arg))
+
+bot.run(Token secreto)
