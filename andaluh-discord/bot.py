50be6ad1dd89d0f97810b51ffe2ea97abb8719a6
J. Félix Ontañón <felixonta@gmail.com>
2020-06-06 20:38:20 +0200

    Added commands for standard (ç) + zezeo + seseo + heheo

--- andaluh-discord/bot.py
+++ andaluh-discord/bot.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 # vim: ts=4
 ###
+# Copyleft 2020 Andalugeeks
+###
 
 import json
 
@@ -9,15 +11,35 @@ import requests
 import discord
 from discord.ext import commands
 
+# Andaluh API. More info: https://andaluh.es
+API_ANDALUH = 'https://api.andaluh.es/epa'
+
+# Discord bot instance
 bot = commands.Bot(command_prefix='/')
 
-@bot.command()
+@bot.command(name='an', help='Type in spanish to get Andalûh EPA transliteration.')
 async def an(ctx, *args):
-    str = requests.get('https://api.andaluh.es/epa', params=dict(spanish=' '.join(args), escapeLinks=True, vaf=u'z')).json()
+    str = requests.get(API_ANDALUH, params=dict(spanish=' '.join(args), escapeLinks=True)).json()
+    await ctx.send(str['andaluh'])
+
+@bot.command(name='anz', help='Type in spanish to get Andalûh EPA Zezeo transliteration.')
+async def anz(ctx, *args):
+    str = requests.get(API_ANDALUH, params=dict(spanish=' '.join(args), escapeLinks=True, vaf=u'z')).json()
+    await ctx.send(str['andaluh'])
+
+@bot.command(name='ans', help='Type in spanish to get Andalûh EPA Seseo transliteration.')
+async def ans(ctx, *args):
+    str = requests.get(API_ANDALUH, params=dict(spanish=' '.join(args), escapeLinks=True, vaf=u's')).json()
+    await ctx.send(str['andaluh'])
+
+@bot.command(name='anh', help='Type in spanish to get Andalûh EPA Heheo transliteration.')
+async def anh(ctx, *args):
+    str = requests.get(API_ANDALUH, params=dict(spanish=' '.join(args), escapeLinks=True, vaf=u'h')).json()
     await ctx.send(str['andaluh'])
 
 if __name__ == '__main__':
-    # Secret token storage as per https://realpython.com/how-to-make-a-discord-bot-python/
+    # Discord secret token storage management
+    # More info: https://realpython.com/how-to-make-a-discord-bot-python/
     import os
     from dotenv import load_dotenv
 
