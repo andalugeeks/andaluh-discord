69763a8dad7b597a23a1ac922ca0aee4ea5239bc
J. Félix Ontañón <felixonta@gmail.com>
2020-06-06 20:13:45 +0200

    Improving code structure

--- andaluh-discord/bot.py
+++ andaluh-discord/bot.py
@@ -1,24 +1,26 @@
-import requests
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: ts=4
+###
+
 import json
+
+import requests
 import discord
 from discord.ext import commands
 
-
 bot = commands.Bot(command_prefix='/')
-
-
-
 
 @bot.command()
 async def an(ctx,p):
     str = requests.get('https://api.andaluh.es/epa', params=dict(spanish= p , escapeLinks=True, vaf=u'z')).json()
     await ctx.send(str['andaluh'])
 
-#prueba de commando
-@bot.command()
-async def foo(ctx, arg):
-    await ctx.send(arg)
-    print(arg)
-    print(type(arg))
+if __name__ == '__main__':
+    # Secret token storage as per https://realpython.com/how-to-make-a-discord-bot-python/
+    import os
+    from dotenv import load_dotenv
 
-bot.run(Token secreto)
+    load_dotenv()
+    TOKEN = os.getenv('DISCORD_TOKEN')
+    bot.run(TOKEN)
